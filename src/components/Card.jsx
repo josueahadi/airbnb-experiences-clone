d51799ce59d2249a7f54a91b194622ff4f1dd398
Josue <ahadihjosue@gmail.com>
2024-07-31 23:28:59 +0200

    feat: card description text - styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,14 +15,14 @@ export default function Card() {
             <div className="relative w-44 md:w-60">
                 <img src={KatieZaferes} className='w-full rounded-xl' alt="Katie Zaferes" />
                 <span className='uppercase text-[#222222] bg-[#FFFFFF] rounded-md px-2 py-1 absolute top-3 left-3'>Sold Out</span>
-                <div className='flex items-center text-base md:text-lg'>
+                <div className='flex items-center text-base md:text-lg mt-2'>
                     <img src={StarIcon} className='w-4 md:w-6' alt="Star Icon" />
                     <span>50.</span>
                     <span className='text-[#918E9B]'>(6)</span>
                     <span className='text-[#918E9B]'>&#8226; USA</span>
                 </div>
-                <p>Life lessons with Katie Zaferes</p>
-                <p><span className='font-bold'>From $136</span> / person</p>
+                <h2 className='mt-2'>Life lessons with Katie Zaferes</h2>
+                <p className='mt-2'><span className='font-bold'>From $136</span> / person</p>
             </div>
         </section>
 
