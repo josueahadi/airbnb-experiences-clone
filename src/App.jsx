708940884b7a5f48a1750ba77b8419f47fb40eab
Josue <ahadihjosue@gmail.com>
2024-08-08 17:48:42 +0200

    chore: dynamically render cards...version 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
 import "./index.css";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/Hero";
-import Card from "./components/Cards";
+import Card from "./components/Card";
+
+// Import images
+import KatieZaferesImage from "./assets/katie-zaferes.png";
+
+const cardsInfo = [
+  {
+    id: 1, // Unique identifier
+    img: KatieZaferesImage,
+    rating: 5.0,
+    reviewCount: 6,
+    country: "USA",
+    title: "Life Lessons with Katie Zaferes",
+    price: 136,
+    isSoldOut: true,
+  },
+  {
+    id: 2, // Unique identifier
+    img: KatieZaferesImage,
+    rating: 4.9,
+    reviewCount: 5,
+    country: "Canada",
+    title: "Learn Cooking with John Doe",
+    price: 120,
+    isSoldOut: false,
+  },
+  {
+    id: 3, // Unique identifier
+    img: KatieZaferesImage,
+    rating: 4.8,
+    reviewCount: 12,
+    country: "UK",
+    title: "Master Photography with Jane Doe",
+    price: 150,
+    isSoldOut: false,
+  },
+];
 
 function App() {
   return (
     <>
       <Navbar />
       <HeroSection />
-      <Card />
+      <section className="flex flex-row pt-16 px-6 sm:px-10 md:px-20">
+        {cardsInfo.map((card) => (
+          <Card
+            key={card.id}
+            img={card.img}
+            rating={card.rating}
+            reviewCount={card.reviewCount}
+            country={card.country}
+            title={card.title}
+            price={card.price}
+            isSoldOut={card.isSoldOut}
+          />
+        ))}
+      </section>
     </>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import StarIcon from "../assets/star.png";
+
+export default function Card(props) {
+  return (
+    <div className="relative w-44 md:w-60">
+      <img src={props.img} className="w-full rounded-xl" alt={props.title} />
+      {props.isSoldOut && (
+        <span className="uppercase text-[#222222] bg-[#FFFFFF] rounded-md px-2 py-1 absolute top-3 left-3">
+          Sold Out
+        </span>
+      )}
+      <div className="flex items-center text-base md:text-lg mt-2">
+        <img src={StarIcon} className="w-4 md:w-6" alt="Star Icon" />
+        <span>{props.rating}</span>
+        <span className="text-[#918E9B]">({props.reviewCount})</span>
+        <span className="text-[#918E9B]">&#8226; {props.country}</span>
+      </div>
+      <h2 className="mt-2">{props.title}</h2>
+      <p className="mt-2">
+        <span className="font-bold">From ${props.price}</span> / person
+      </p>
+    </div>
+  );
+}
