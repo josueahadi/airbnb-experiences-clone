e1a615eb02a5129c13a1ca94facf2aa040492894
Josue <ahadihjosue@gmail.com>
2024-08-08 17:46:03 +0200

    render cards dynamically....version 1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,22 @@
-import experiencesGrid from '../assets/photo-grid.png'
+import experiencesGrid from "../assets/photo-grid.png";
 
 export default function Hero() {
-    return (
-        <section className='w-full'>
-            <div className='flex justify-center'>
-                <img src={experiencesGrid} className='w-2/3 py-10' alt="Experience Grid" />
-            </div>
-            <div className='text-left px-20'>
-                <h1 className='font-semibold text-4xl'>Online Experiences</h1>
-                <p className='font-light text-base md:text-2xl pt-3 md:w-3/5'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
-                </p>
-            </div>
-        </section>
-    )
+  return (
+    <section className="w-full px-6 sm:px-10 md:px-20">
+      <div className="flex justify-center">
+        <img
+          src={experiencesGrid}
+          className="w-full sm:w-full md:w-full lg:w-2/3 py-10"
+          alt="Experience Grid"
+        />
+      </div>
+      <div className="text-left">
+        <h1 className="font-semibold text-4xl">Online Experiences</h1>
+        <p className="font-light text-base md:text-2xl pt-3 md:w-[28rem]">
+          Join unique interactive activities led by one-of-a-kind hosts—all
+          without leaving home.
+        </p>
+      </div>
+    </section>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
-import airbnbLogo from '../assets/airbnb-logo.png'
+import airbnbLogo from "../assets/airbnb-logo.png";
 
 export default function Header() {
-    return (
-        <header className='w-full'>
-            <nav className='px-20 py-8 shadow-custom w-full'>
-                <a href="#" target="_blank">
-                    <img src={airbnbLogo} className="w-24" alt="Airbnb Logo" />
-                </a>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="w-full">
+      <nav className="px-6 sm:px-10 md:px-20 py-8 shadow-custom w-full">
+        <a href="#" target="_blank">
+          <img src={airbnbLogo} className="w-24" alt="Airbnb Logo" />
+        </a>
+      </nav>
+    </header>
+  );
 }
